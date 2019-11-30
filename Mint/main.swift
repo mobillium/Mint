18ab51c5f1f14b16ffdf5c675a70771802b10798
Mustafa GUNES <gunes149@gmail.com>
2019-11-30 16:24:59 +0300

    Initial Commit

---
+++ Mint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mint
+//
+//  Created by Mustafa GUNES on 30.11.2019.
+//  Copyright © 2019 Mustafa GUNES. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
